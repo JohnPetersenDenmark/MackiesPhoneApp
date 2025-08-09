4c9984595585e1ca7fe9dc66f1ceebec6a8e724f
JohnPetersenDenmark <johnpetersen1959@gmail.com>
2025-08-09 16:18:18 +0200

    ok

--- MackiesPhoneApp/Pages/User/CustomFlyoutPageV2.xaml.cs
+++ MackiesPhoneApp/Pages/User/CustomFlyoutPageV2.xaml.cs
@@ -1,75 +1,82 @@
-namespace MackiesPhoneApp.Pages.User;
+using Microsoft.Maui.Controls;
+using MackiesPhoneApp.Pages.User;
 
-public partial class CustomFlyoutPageV2 : FlyoutPage
+namespace MackiesPhoneApp.Pages.User
 {
-    bool _isInitialized = false;
-
-
-    public CustomFlyoutPageV2()
-	{
-		InitializeComponent();
-
-       
-        Detail = new NavigationPage(new HomePage());
-
-        Appearing += OnAppearing;
-        Disappearing += OnDisappearing;
-    }
-
-    public void SetMainPage(Page page)
+    public partial class CustomFlyoutPageV2 : FlyoutPage
     {
-        Detail = new NavigationPage(page);
-        IsPresented = false; // Closes the menu
-    }
+        private NavigationPage _navPage;
+        private bool _isInitialized = false;
 
-    private void GoHome_Clicked(object sender, EventArgs e)
-    {
-        Detail = new NavigationPage(new HomePage());
-
-     
-        IsPresented = false; // Close the flyout
-    }
-
-    private void GoLogin_Clicked(object sender, EventArgs e)
-    {
-        Detail = new NavigationPage(new LoginPage());
-        IsPresented = false;
-    }
-
-    private void GoSettings_Clicked(object sender, EventArgs e)
-    {
-        Detail = new NavigationPage(new SettingsPage());
-        IsPresented = false;
-    }
-
-    private void OnAppearing(object sender, EventArgs e)
-    {
-        if (!_isInitialized)
+        public CustomFlyoutPageV2()
         {
-            ApplyTheme(App.Current.RequestedTheme);
-            App.Current.RequestedThemeChanged += OnRequestedThemeChanged;
-            _isInitialized = true;
+            InitializeComponent();
+
+            _navPage = Detail as NavigationPage;        
+           
+            Appearing += CustomFlyoutPageV2_Appearing;
+            Disappearing += CustomFlyoutPageV2_Disappearing;
         }
-    }
+         
 
-    private void OnDisappearing(object sender, EventArgs e)
-    {
-        App.Current.RequestedThemeChanged -= OnRequestedThemeChanged;
-    }
+        private void CustomFlyoutPageV2_Disappearing(object? sender, EventArgs e)
+        {
+            base.OnDisappearing();
+            App.Current.RequestedThemeChanged -= OnRequestedThemeChanged;
+        }
 
-    private void OnRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
-    {
-        ApplyTheme(e.RequestedTheme);
-    }
+        private void CustomFlyoutPageV2_Appearing(object? sender, EventArgs e)
+        {
+            base.OnAppearing();
 
-    private void ApplyTheme(AppTheme theme)
-    {
-        SetSystemBarsColors(theme);
-        SetToolbarColors(theme);
-    }
+            if (!_isInitialized)
+            {
+                ApplyTheme(App.Current.RequestedTheme);
+                App.Current.RequestedThemeChanged += OnRequestedThemeChanged;
+                _isInitialized = true;
+            }
+        }
 
-    private void SetSystemBarsColors(AppTheme theme)
-    {
+      
+        // Navigation methods
+        private void GoHome_Clicked(object sender, EventArgs e)
+        {
+            SetRootPage(new HomePage());
+        }
+
+        private void GoLogin_Clicked(object sender, EventArgs e)
+        {
+            SetRootPage(new LoginPage());
+        }
+
+        private void GoSettings_Clicked(object sender, EventArgs e)
+        {
+            SetRootPage(new SettingsPage());
+        }
+
+        private void SetRootPage(Page newRoot)
+        {
+            // Replace root without replacing NavigationPage
+            _navPage.Navigation.InsertPageBefore(newRoot, _navPage.RootPage);
+            _navPage.PopToRootAsync(false);
+
+            IsPresented = false; // Close flyout
+        }
+
+      
+        private void OnRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+        {
+            ApplyTheme(e.RequestedTheme);
+        }
+
+        private void ApplyTheme(AppTheme theme)
+        {
+            SetSystemBarsColors(theme);
+            SetToolbarColors(theme);
+        }
+
+        private void SetSystemBarsColors(AppTheme theme)
+        {
 #if ANDROID
         var colorText = theme == AppTheme.Dark ? "AppSystemBarsBackgroundColorDark" : "AppSystemBarsBackgroundColorLight";
 
@@ -78,27 +85,28 @@ public partial class CustomFlyoutPageV2 : FlyoutPage
             MackiesPhoneApp.Platforms.Android.PlatformStatusBarHelper.SetSystemBarsColor(backgroundColor);
         }
 #endif
-    }
+        }
 
-    private void SetToolbarColors(AppTheme theme)
-    {
-        if (Detail is NavigationPage navPage)
+        private void SetToolbarColors(AppTheme theme)
         {
-            if (theme == AppTheme.Dark)
+            if (Detail is NavigationPage navPage)
             {
-                if (Application.Current.Resources.TryGetValue("AppToolbarBackgroundColorDark", out var bg) && bg is Color bgColor)
-                    navPage.BarBackgroundColor = bgColor;
+                if (theme == AppTheme.Dark)
+                {
+                    if (Application.Current.Resources.TryGetValue("AppToolbarBackgroundColorDark", out var bg) && bg is Color bgColor)
+                        navPage.BarBackgroundColor = bgColor;
 
-                if (Application.Current.Resources.TryGetValue("AppToolbarTextColorDark", out var txt) && txt is Color txtColor)
-                    navPage.BarTextColor = txtColor;
-            }
-            else
-            {
-                if (Application.Current.Resources.TryGetValue("AppToolbarBackgroundColorLight", out var bg) && bg is Color bgColor)
-                    navPage.BarBackgroundColor = bgColor;
+                    if (Application.Current.Resources.TryGetValue("AppToolbarTextColorDark", out var txt) && txt is Color txtColor)
+                        navPage.BarTextColor = txtColor;
+                }
+                else
+                {
+                    if (Application.Current.Resources.TryGetValue("AppToolbarBackgroundColorLight", out var bg) && bg is Color bgColor)
+                        navPage.BarBackgroundColor = bgColor;
 
-                if (Application.Current.Resources.TryGetValue("AppToolbarTextColorLight", out var txt) && txt is Color txtColor)
-                    navPage.BarTextColor = txtColor;
+                    if (Application.Current.Resources.TryGetValue("AppToolbarTextColorLight", out var txt) && txt is Color txtColor)
+                        navPage.BarTextColor = txtColor;
+                }
             }
         }
     }
